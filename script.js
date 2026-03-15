23b47686ba0649af52c3bcf4e9b31b2093b2eac0
vyrval <erujunangelo@gmail.com>
2026-03-15 14:10:09 +0800

    Fix spacing in response message

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "i like you too!♡";
+  question.innerHTML = "i like you too! ♡";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
